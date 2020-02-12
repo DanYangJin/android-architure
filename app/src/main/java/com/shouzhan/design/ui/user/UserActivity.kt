14f8231a4bb05fc715bac8e9ad0f6e282c68ad79
danbin <android1410danbin@163.com>
2020-02-12 20:00:04 +0800

    bugfix

--- app/src/main/java/com/shouzhan/design/ui/user/UserActivity.kt
+++ app/src/main/java/com/shouzhan/design/ui/user/UserActivity.kt
@@ -1,9 +1,16 @@
 package com.shouzhan.design.ui.user
 
+import android.animation.ObjectAnimator
 import android.content.Intent
+import android.util.Log
+import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import com.fshows.android.parker.navigation.GlueTabLayout
 import com.shouzhan.design.R
 import com.shouzhan.design.adapter.HomePagerAdapter
 import com.shouzhan.design.base.BaseActivity
@@ -13,6 +20,7 @@ import com.shouzhan.design.ui.login.LoginActivity
 import com.shouzhan.design.ui.mvp.MvpActivity
 import kotlinx.android.synthetic.main.activity_user.*
 
+
 /**
  * @author danbin
  * @version KotlinActivity.java, v 0.1 2019-02-26 上午11:27 danbin
@@ -20,18 +28,84 @@ import kotlinx.android.synthetic.main.activity_user.*
  */
 class UserActivity : BaseActivity<ActivityUserBinding>() {
 
-    private var titles = mutableListOf("测试1", "测试2")
+    private var titles = mutableListOf("我的设备", "设备商城")
 
     override fun getLayoutId(): Int {
         return R.layout.activity_user
     }
 
     override fun initView() {
+        home_tab_layout.addOnTabSelectedListener(object : GlueTabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: GlueTabLayout.Tab) {
+                Log.e("xsd", "onTabSelected: ${tab.position}")
+                var view = tab.customView
+                view?.run {
+                    val tabIcon: ImageView = findViewById(R.id.tab_icon)
+                    val tabName: TextView = findViewById(R.id.tab_name)
+                    tabName.setTextColor(ContextCompat.getColor(mContext, R.color.color_red_f04144))
+                    when (tab.position) {
+                        1 -> {
+                            tabIcon.setImageResource(R.mipmap.ic_arrow_down_red)
+                        }
+                    }
+                }
+            }
+
+            override fun onTabUnselected(tab: GlueTabLayout.Tab) {
+                Log.e("xsd", "onTabUnselected: ${tab.position}")
+                var view = tab.customView
+                view?.run {
+                    val tabIcon: ImageView = findViewById(R.id.tab_icon)
+                    val tabName: TextView = findViewById(R.id.tab_name)
+                    tabName.setTextColor(ContextCompat.getColor(mContext, R.color.color_666))
+                    when (tab.position) {
+                        1 -> {
+                            tabIcon.setImageResource(R.mipmap.ic_arrow_down_black)
+                        }
+                    }
+                }
+            }
+
+            var isVisible = false
+            override fun onTabReselected(tab: GlueTabLayout.Tab) {
+                Log.e("xsd", "onTabReselected: ${tab.position}")
+                var view = tab.customView
+                view?.run {
+                    val tabIcon: ImageView = findViewById(R.id.tab_icon)
+                    when (tab.position) {
+                        1 -> {
+                            isVisible = !isVisible
+                            tabIcon.clearAnimation()
+                            val animator = ObjectAnimator.ofFloat(tabIcon, "rotation", if (isVisible) 0f else 180f, if (isVisible) 180f else 360f)
+                            animator.duration = 300
+                            animator.start()
+                        }
+                    }
+                }
+            }
+        })
+        home_tab_layout.setSlidingIndicatorAnimType(GlueTabLayout.AnimType.GLUE)
+        home_tab_layout.setTabIndicatorWidth(0.3f)
         home_tab_layout.setupWithViewPager(home_view_pager)
         var adapter = HomePagerAdapter(supportFragmentManager)
         home_view_pager.adapter = adapter
         repeat(titles.size) {
-            home_tab_layout.getTabAt(it)?.text = titles[it]
+            val tab = home_tab_layout.getTabAt(it)
+            val view = LayoutInflater.from(this).inflate(R.layout.item_tab, home_tab_layout, false)
+            val tabIcon: ImageView = view.findViewById(R.id.tab_icon)
+            val tabName: TextView = view.findViewById(R.id.tab_name)
+            tabName.text = titles[it]
+            tabName.setTextColor(if (it == 0) {
+                ContextCompat.getColor(mContext, R.color.color_f04144)
+            } else {
+                ContextCompat.getColor(mContext, R.color.color_666)
+            })
+            tabIcon.visibility = if (it == 0) {
+                View.GONE
+            } else {
+                View.VISIBLE
+            }
+            tab?.customView = view
         }
     }
 
@@ -40,19 +114,19 @@ class UserActivity : BaseActivity<ActivityUserBinding>() {
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu,menu)
+        menuInflater.inflate(com.shouzhan.design.R.menu.menu, menu)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
-        when(item!!.itemId) {
-            R.id.id_login -> {
+        when (item!!.itemId) {
+            com.shouzhan.design.R.id.id_login -> {
                 startActivity(Intent(mContext, LoginActivity::class.java))
             }
-            R.id.id_flex_box -> {
+            com.shouzhan.design.R.id.id_flex_box -> {
                 startActivity(Intent(mContext, FlexboxActivity::class.java))
             }
-            R.id.id_dragger -> {
+            com.shouzhan.design.R.id.id_dragger -> {
                 startActivity(Intent(mContext, MvpActivity::class.java))
             }
         }
