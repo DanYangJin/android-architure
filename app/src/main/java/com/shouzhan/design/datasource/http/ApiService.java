f6ba964d6c02001ab7a0b9a20ad6edb19a7acc3e
danbin <android1410danbin@163.com>
2020-12-19 16:34:30 +0800

    bugfix去掉多余请求

--- app/src/main/java/com/shouzhan/design/datasource/http/ApiService.java
+++ app/src/main/java/com/shouzhan/design/datasource/http/ApiService.java
@@ -20,21 +20,11 @@ import retrofit2.http.Query;
 public interface ApiService {
 
     class Builder {
-        public static ApiService getPhpServer() {
-            return ServiceBuildFactory.getInstance().create(ApiService.class, HttpConstants.HOST, ServiceBuildFactory.PHP_HOST);
-        }
-
-        public static ApiService getJavaServer() {
-            return ServiceBuildFactory.getInstance().create(ApiService.class, HttpConstants.JAVA_HOST, ServiceBuildFactory.JAVA_HOST);
-        }
-
-        public static ApiService getJavaLoanServer() {
-            return ServiceBuildFactory.getInstance().create(ApiService.class, HttpConstants.JAVA_LOAN_HOST, ServiceBuildFactory.JAVA_LOAN_HOST);
-        }
 
         public static ApiService getJavaPerformanceServer() {
             return ServiceBuildFactory.getInstance().create(ApiService.class, HttpConstants.JAVA_PERFORMANCE_HOST, ServiceBuildFactory.JAVA_PERFORMANCE_HOST);
         }
+
     }
 
     /**
